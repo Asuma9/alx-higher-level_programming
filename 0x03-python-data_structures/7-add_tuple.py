d91e2a6eb64bc6777be2582be86f63813f7c51a1
Asuma9 <paulasuma10@gmail.com>
2023-11-12 17:16:02 +0300

    function that adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-#6-print_matrix_integer.py
+# 6-print_matrix_integer.py
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-	"""adds 2 tuples."""
-	
-	# handle tuple_a
-	if len(tuple_a) == 1:
-		tuple_a = (tuple_a[0], 0)
-	elif len(tuple_a) == 0:
-		tuple_a = (0,0)
+    """adds 2 tuples."""
+    # handle tuple_a
+    if len(tuple_a) == 1:
+        tuple_a = (tuple_a[0], 0)
+        elif len(tuple_a) == 0:
+            tuple_a = (0, 0)
 
-	#Handle tuple_b
-	if len(tuple_b) == 1:
-		tuple_b = (tuple_b[0], 0)
-	elif len(tuple_b) == 0:
-		tuple_b = (0,0)
+        # Handle tuple_b
+        if len(tuple_b) == 1:
+            tuple_b = (tuple_b[0], 0)
+        elif len(tuple_b) == 0:
+            tuple_b = (0, 0)
 
-	#tuple addition 
-	tuple_sum = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-	return (tuple_sum)
+        # tuple addition
+        tuple_sum = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+        return (tuple_sum)
