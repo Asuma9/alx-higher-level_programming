62343276e8f5e058d11ec0e2b99887b7a00e8acc
Asuma9 <paulasuma10@gmail.com>
2023-11-12 17:04:35 +0300

    Replace an item at a given index without modifying the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-#4-new_in_list.py
+# 4-new_in_list.py
 
 
 def new_in_list(my_list, idx, element):
-	"""replaces an element in a list at a specific position without modifying the original list"""
-	new_list = my_list[:]
-	if idx < 0 or idx > (len(my_list) - 1):
-		return (new_list)
-	new_list[idx] = element
-	return (new_list)
+    """Replace an item at a given index without modifying the original list"""
+    new_list = my_list[:]
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (new_list)
+    new_list[idx] = element
+    return (new_list)
