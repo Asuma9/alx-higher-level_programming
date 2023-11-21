00ba566f916f1a58361bec50fec749ea10756ba7
Asuma9 <paulasuma10@gmail.com>
2023-11-21 19:10:39 +0300

    function that prints the first x elements

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    """
+
+    Args:
+        my_list (list): list to print from.
+        x (int): number of elements of my_list to print.
+
+    Returns:
+        number of elements printed.
+
+    """
+
+    try:
+        count = 0
+        for element in my_list[:x]:
+            if count == x:
+                break
+            if type(element) == int:
+                print("{:d}".format(element), end='')
+                count += 1
+        print()
+        return count
+    except TypeError:
+        return count
