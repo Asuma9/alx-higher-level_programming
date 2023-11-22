1cba51d6ddaec74077b87791b64a798b66f76b20
Asuma9 <paulasuma10@gmail.com>
2023-11-22 16:16:25 +0300

    function that divides element by element 2 lists.

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-def list_division(my_list_1, my_list_2, list_lenght):
+def list_division(my_list_1, my_list_2, list_length):
     """ Divides elementwise between two list
 
     Args:
