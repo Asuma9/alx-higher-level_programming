a73147734e17e874cdfcca1d225062ee6646ac62
Asuma9 <paulasuma10@gmail.com>
2023-11-22 16:56:30 +0300

    Python magic function to display bytecode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
 
     for attempt in range(0, max_attempts):
         try:
-            if attempt > a:
+            if (attempt > a):
                 raise Exception("Too far")
             else:
                 result += (a ** b) / attempt
