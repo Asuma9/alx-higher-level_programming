6d8c6cdfa93c7380845a98a2c10da7c8d191eb69
Asuma9 <paulasuma10@gmail.com>
2023-11-22 16:48:17 +0300

    Python magic function to display bytecode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,8 +3,20 @@ import dis
 
 
 def magic_calculation(a, b):
-    result = (a ** 2) * (b * 5)
-    return result
+    result = 0
+    max_attempts = 3
 
-#Display the bytecode of the function
-dis.dis(magic_calculation)
+    for attemp in range(0, max_attempts + 1):
+        try:
+            if attempt > a:
+                raise Exception("Too far")
+            else:
+                result += (a ** b) / attempt
+        except Exception as e:
+            print(f"Exception: {e}")
+            result = b + a
+            break
+    else:
+        print("No exception occurred during the loop.")
+
+    return result
