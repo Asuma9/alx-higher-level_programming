ab4b7dd922f859d6604a05c541912043b87216a7
Asuma9 <paulasuma10@gmail.com>
2023-11-22 19:22:00 +0300

    functions that print some basic info about Python lists, Python bytes an Python float objects.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -23,5 +23,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
         print()
         return count
-    except TypeError:
-        return count
+    except (TypeError, IndexError):
+        print()
+        raise
