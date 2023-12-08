62e5c5b6570868721b7e7e323009f5bbbdef10d3
Asuma9 <paulasuma10@gmail.com>
2023-12-08 12:17:23 +0300

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Creates an improved class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Defines an object Basegeometry to return an are"""
+    def area(self):
+        """Method to return an area"""
+        raise Exception("area() is not implemented")
