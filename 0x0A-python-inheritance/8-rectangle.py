307e407d22f2fed16b80f9f3fb464c396a63bd9b
Asuma9 <paulasuma10@gmail.com>
2023-12-13 16:29:40 +0300

    8. Rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -14,14 +14,14 @@ class BaseGeometry:
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
-
 class Rectangle(BaseGeometry):
     """Defines a subclass of BaseGeometry class"""
 
     def __init__(self, width, height):
         """Instantiation of Rectangle obj attributes"""
 
+        super().__init__() # Call to constructor  of the parent class
         self.__width = width
         self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
