3085f455d313a26707cf91285fa872227d429000
Asuma9 <paulasuma10@gmail.com>
2023-12-14 19:12:15 +0300

    13. Can I?

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -11,5 +11,7 @@ def add_attribute(obj, attr_name, attr_value):
 
     if hasattr(obj, "__dict__") or isinstance(obj, dict):
         """Checks for dict attribute or its instance b4 adding sn attribute"""
-        raise TypeError("Can't add new attribute")
-    setattr(obj, attr_name, attr_value)
+        obj.__setattr__(attr_name, attr_value)
+    else:
+        raise TypeError("can't add new attribute")
+    
