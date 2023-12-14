b2972efe34047306d05f7007c7fd46346d78fb51
Asuma9 <paulasuma10@gmail.com>
2023-12-14 05:37:01 +0300

    8. Rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -10,7 +10,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Checks if object is an integer"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
@@ -21,7 +21,6 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """Instantiation of Rectangle obj attributes"""
-
         self.__width = width
         self.__height = height
         self.integer_validator("width", width)
