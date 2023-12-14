e71daaa96a35d9834eb32dbbea4e7f77900415dc
Asuma9 <paulasuma10@gmail.com>
2023-12-14 17:16:25 +0300

    11. Square #2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -42,10 +42,6 @@ class Square(Rectangle):
         super().__init__(size, size)
         self.__size = size
 
-    def area(self):
-        """Customize area method for the square"""
-        return self.__size * self.__size
-
     def __str__(self):
-        """Prints Square attributes"""
+        """overriding __str__ to print customized string rep for square"""
         return "[Square] {}/{}".format(self.__size, self.__size)
